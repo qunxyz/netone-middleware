aff03422720bb9c6c04679499f3870db132fd2bb
oesee@139.com <oesee@139.com@f0528040-21d0-c4c1-14c2-750bfacab96f>
2011-04-27 03:46:47 +0000

    增强子流程调度，自动同步父流程的数据

--- trunk/ netone-middleware/wfweb/engine/oe/midware/workflow/engine/actor/SubProcessActorImp.java
+++ trunk/ netone-middleware/wfweb/engine/oe/midware/workflow/engine/actor/SubProcessActorImp.java
@@ -1,23 +1,27 @@
 package oe.midware.workflow.engine.actor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import oe.frame.bus.workflow.ActivityExtendAttribute;
+import oe.frame.bus.workflow.ProcessEngine;
 import oe.frame.bus.workflow.RuntimeMonitor;
 import oe.frame.bus.workflow.WfEntry;
 import oe.frame.bus.workflow.actor.ProcessActor;
 import oe.frame.bus.workflow.actor.SubProcessActor;
 import oe.frame.bus.workflow.actor.activity.ActivityStatusActor;
 import oe.frame.orm.OrmerEntry;
-import oe.midware.workflow.XMLException;
 import oe.midware.workflow.runtime.ActivityRef;
 import oe.midware.workflow.runtime.RuntimeProcessRef;
 import oe.midware.workflow.runtime.RuntimeWorklistRef;
 import oe.midware.workflow.runtime.XpdlModelHandler;
+import oe.midware.workflow.runtime.ormobj.TWfRelevantvar;
 import oe.midware.workflow.runtime.ormobj.TWfRuntime;
 import oe.midware.workflow.runtime.ormobj.TWfWorklist;
 import oe.midware.workflow.xpdl.model.activity.Implementation;
 import oe.midware.workflow.xpdl.model.activity.SubFlow;
+import oe.midware.workflow.xpdl.model.data.DataField;
 import oe.midware.workflow.xpdl.model.workflow.WorkflowProcess;
 
 import org.apache.commons.logging.Log;
@@ -44,9 +48,11 @@ public class SubProcessActorImp implements SubProcessActor {
 
 		RuntimeMonitor runtimemonitor = (RuntimeMonitor) WfEntry
 				.fetchBean("runtimemonitor");
+
+		String processid = null;
 		try {
-			String processid = runtimemonitor.loadRuntime(
-					curWorklist.getRuntimeid()).getProcessid();
+			processid = runtimemonitor.loadRuntime(curWorklist.getRuntimeid())
+					.getProcessid();
 			Map mapExtend = runtimemonitor.fetchActivity(processid,
 					curWorklist.getActivityid()).getExtendedAttributes()
 					.getMap();
@@ -90,17 +96,71 @@ public class SubProcessActorImp implements SubProcessActor {
 				+ procsub.getId());
 		TWfRuntime runtimeSub = processActor.newProcess(procsub);
 
+		// 增加子流程自动同步父流程的雷同的相关变量
+		syncRelevantvar(runtimeSub, curWorklist, procsub, processid);
+
 		// 添加子流程的隶属信息
 		runtimeSub.setBelongactivityid(curWorklist.getWorkcode());
 		runtimeSub.setBelongruntimeid(curWorklist.getRuntimeid());
 		runtimeSub.setKind(subflowSyncmode);
 
 		OrmerEntry.fetchOrmer().fetchSerializer().update(runtimeSub);
-		
-		//马上启动子流程
+
+		// 马上启动子流程
 		processActor.runProcess(runtimeSub);
 
 		return runtimeSub;
+	}
+
+	private void syncRelevantvar(TWfRuntime runtimeSub,
+			TWfWorklist curWorklist, WorkflowProcess procsub,
+			String processidFather) {
+		RuntimeMonitor runtimemonitor = (RuntimeMonitor) WfEntry
+				.fetchBean("runtimemonitor");
+
+		ProcessEngine processEngine = (ProcessEngine) WfEntry
+				.fetchBean("processEngine");
+
+		WorkflowProcess procFather = ((XpdlModelHandler) WfEntry
+				.fetchBean("xpdlModelHandler"))
+				.fetchXpdlWorkflowByProcessid(processidFather);
+
+		DataField[] dataFather = procFather.getDataField();
+		
+		if(_log.isDebugEnabled()){
+			_log.debug("fatherData List");
+			for (int i = 0; i < dataFather.length; i++) {
+				_log.debug(dataFather[i].getName());
+			}
+		}
+
+		// 获得子流程的ID
+		String subflowRuntimeid = runtimeSub.getRuntimeid();
+		// 获得父流程的ID
+		String fatherflowRuntimeId = curWorklist.getRuntimeid();
+
+		List syncData = new ArrayList();
+		try {
+			for (int i = 0; i < dataFather.length; i++) {
+				String dataname = dataFather[i].getName();
+				DataField subObject = procsub.getDataField(dataname);
+				_log.debug("check sub data:"+dataname);
+				if (subObject != null) {// 检查子流程是否存在与父流程雷同的变量
+					_log.debug("check sub data exist:"+dataname);
+					TWfRelevantvar revSub = runtimemonitor.fetchRelevantVar(
+							subflowRuntimeid, dataname);
+					TWfRelevantvar revFather = runtimemonitor.fetchRelevantVar(
+							fatherflowRuntimeId, dataname);
+					String fatherdataValue=revFather.getValuenow();
+					_log.debug("add to sub data:"+fatherdataValue);
+					revSub.setValuenow(revFather.getValuenow());// 将父流程的该变量值写入子流程变量中
+					syncData.add(revSub);// 记录下，要做数据同步，
+				}
+			}
+			processEngine.updateRelevantvars(syncData);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private String fetchExecuteType(SubFlow subflow) {
--- trunk/ netone-middleware/wfweb/engine/oe/midware/workflow/engine/actor/SubProcessActorTmpImp.java
+++ trunk/ netone-middleware/wfweb/engine/oe/midware/workflow/engine/actor/SubProcessActorTmpImp.java
@@ -1,23 +1,27 @@
 package oe.midware.workflow.engine.actor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import oe.frame.bus.workflow.ActivityExtendAttribute;
+import oe.frame.bus.workflow.ProcessEngine;
 import oe.frame.bus.workflow.RuntimeMonitor;
 import oe.frame.bus.workflow.WfEntry;
 import oe.frame.bus.workflow.actor.ProcessActor;
 import oe.frame.bus.workflow.actor.SubProcessActor;
 import oe.frame.bus.workflow.actor.activity.ActivityStatusActor;
 import oe.frame.orm.OrmerEntry;
-import oe.midware.workflow.XMLException;
 import oe.midware.workflow.runtime.ActivityRef;
 import oe.midware.workflow.runtime.RuntimeProcessRef;
 import oe.midware.workflow.runtime.RuntimeWorklistRef;
 import oe.midware.workflow.runtime.XpdlModelHandler;
+import oe.midware.workflow.runtime.ormobj.TWfRelevantvar;
 import oe.midware.workflow.runtime.ormobj.TWfRuntime;
 import oe.midware.workflow.runtime.ormobj.TWfWorklist;
 import oe.midware.workflow.xpdl.model.activity.Implementation;
 import oe.midware.workflow.xpdl.model.activity.SubFlow;
+import oe.midware.workflow.xpdl.model.data.DataField;
 import oe.midware.workflow.xpdl.model.workflow.WorkflowProcess;
 
 import org.apache.commons.logging.Log;
@@ -43,8 +47,9 @@ public class SubProcessActorTmpImp implements SubProcessActor {
 		String subflowSyncmode = null;
 		RuntimeMonitor runtimemonitor = (RuntimeMonitor) WfEntry
 				.fetchBean("runtimemonitor");
+		String processid =null;
 		try {
-			String processid = runtimemonitor.loadRuntime(
+			processid = runtimemonitor.loadRuntime(
 					curWorklist.getRuntimeid()).getProcessid();
 			Map mapExtend = runtimemonitor.fetchActivity(processid,
 					curWorklist.getActivityid()).getExtendedAttributes()
@@ -77,7 +82,9 @@ public class SubProcessActorTmpImp implements SubProcessActor {
 			curWorklist
 					.setExecutestatus(RuntimeWorklistRef.STATUS_SUBFLOW_STOP[0]);
 		} else {
-			curWorklist.setExecutestatus(RuntimeWorklistRef.STATUS_RUNNING[0]);
+			curWorklist.setExecutestatus(RuntimeWorklistRef.STATUS_QUASH[0]);
+			// 如果是异步子流程那么直接提交，父节点
+			//processActor.commitActivity(curWorklist);
 		}
 
 		OrmerEntry.fetchOrmer().fetchSerializer().update(curWorklist);
@@ -88,6 +95,10 @@ public class SubProcessActorTmpImp implements SubProcessActor {
 		_log.debug("start subflow:" + procsub.getName() + "ID:"
 				+ procsub.getId());
 		TWfRuntime runtimeSub = processActor.newProcess(procsub);
+		
+		// 增加子流程自动同步父流程的雷同的相关变量
+		syncRelevantvar(runtimeSub, curWorklist, procsub, processid);
+		
 		// 添加子流程的隶属信息
 		runtimeSub.setBelongactivityid(curWorklist.getWorkcode());
 		runtimeSub.setBelongruntimeid(curWorklist.getRuntimeid());
@@ -99,6 +110,58 @@ public class SubProcessActorTmpImp implements SubProcessActor {
 		processActor.runProcess(runtimeSub);
 
 		return runtimeSub;
+	}
+	
+	
+	private void syncRelevantvar(TWfRuntime runtimeSub,
+			TWfWorklist curWorklist, WorkflowProcess procsub,
+			String processidFather) {
+		RuntimeMonitor runtimemonitor = (RuntimeMonitor) WfEntry
+				.fetchBean("runtimemonitor");
+
+		ProcessEngine processEngine = (ProcessEngine) WfEntry
+				.fetchBean("processEngine");
+
+		WorkflowProcess procFather = ((XpdlModelHandler) WfEntry
+				.fetchBean("xpdlModelHandler"))
+				.fetchXpdlWorkflowByProcessid(processidFather);
+
+		DataField[] dataFather = procFather.getDataField();
+		
+		if(_log.isDebugEnabled()){
+			_log.debug("fatherData List");
+			for (int i = 0; i < dataFather.length; i++) {
+				_log.debug(dataFather[i].getName());
+			}
+		}
+
+		// 获得子流程的ID
+		String subflowRuntimeid = runtimeSub.getRuntimeid();
+		// 获得父流程的ID
+		String fatherflowRuntimeId = curWorklist.getRuntimeid();
+
+		List syncData = new ArrayList();
+		try {
+			for (int i = 0; i < dataFather.length; i++) {
+				String dataid = dataFather[i].getId();
+				DataField subObject = procsub.getDataField(dataid);
+				_log.debug("check sub data:"+dataid);
+				if (subObject != null) {// 检查子流程是否存在与父流程雷同的变量
+					_log.debug("check sub data exist:"+dataid);
+					TWfRelevantvar revSub = runtimemonitor.fetchRelevantVar(
+							subflowRuntimeid, dataid);
+					TWfRelevantvar revFather = runtimemonitor.fetchRelevantVar(
+							fatherflowRuntimeId, dataid);
+					String fatherdataValue=revFather.getValuenow();
+					_log.debug("add to sub data:"+fatherdataValue);
+					revSub.setValuenow(revFather.getValuenow());// 将父流程的该变量值写入子流程变量中
+					syncData.add(revSub);// 记录下，要做数据同步，
+				}
+			}
+			processEngine.updateRelevantvars(syncData);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private String fetchExecuteType(SubFlow subflow) {
